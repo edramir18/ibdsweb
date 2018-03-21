8020c6811edb7ac862cd24a10a54d59eed0e1479
Eduardo Ramirez <edramir18@gmail.com>
2018-03-21 14:20:29 -0400

    SQL Full Backup

---
+++ sql/export_categoria.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  File created - Wednesday-March-21-2018   
+--------------------------------------------------------
+REM INSERTING into IBDS.CATEGORIA
+SET DEFINE OFF;
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (1,'Logout',-1,null,'255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (2,'Login',0,null,'255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (3,'Transferencias',1,'-1','255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (4,'Pagos',2,'-1','255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (5,'Consolidado',3,'-1','255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (6,'Detalles',4,'-1','255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (7,'Otros',5,'-1','255, 225, 25');
+Insert into IBDS.CATEGORIA (ID,NOMBRE,ORDEN,FILL,COLOR) values (0,'None',null,null,null);
